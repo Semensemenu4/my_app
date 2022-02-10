b8fa00bf546e2bd1cf8cb5d55e551c6d46ebfc09
Sergey Otchenko <otchenko@reg.ru>
2022-02-10 15:10:51 +0200

    add 404 rout

--- main/short_url/views.py
+++ main/short_url/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+
+def index( request ):
+    return HttpResponse( '<h1> MAIN </h1>' )
+
+def pageNotFound( request, exception ):
+    return HttpResponseNotFound( '<h1> 404 </h1>' )
