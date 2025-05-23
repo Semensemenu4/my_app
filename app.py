1fc3183fdd46a6d51d474a1b9774c2a7d4d99ae7
Serhii Otchenko <sotchenko1986@gmail.com>
2025-05-22 22:19:12 -0700

    add 404 page

--- app.py
+++ app.py
@@ -21,3 +21,9 @@ def load_user(user_id):
 # Регистрируем блюпринты
 app.register_blueprint(auth_bp)
 app.register_blueprint(shortener_bp)
+
+from flask import render_template
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
